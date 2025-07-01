69b948043f427c4b32e1ce9e414283835eea861c
gokamoda <gokamoda@users.noreply.github.com>
2025-07-01 09:50:41 +0000

    deploy: e449417dfc82fbdc879b8d3d470309eca9652d9d

---
+++ docs/preview/pr-7/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Go_kamoda",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/pages-preview-test/preview/pr-7/members/go_kamoda/",
+        "teaser": null
+      },{
+        "title": "Sho_yokoi",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/pages-preview-test/preview/pr-7/members/sho_yokoi/",
+        "teaser": null
+      },{
+        "title": "Yuji_yamamoto",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/pages-preview-test/preview/pr-7/members/yuji_yamamoto/",
+        "teaser": null
+      },{
+        "title": "Kickoff!",
+        "excerpt":"横井研発足  国語研で立ち上げました  ","categories": ["News"],
+        "tags": [],
+        "url": "/pages-preview-test/preview/pr-7/news/kickoff/",
+        "teaser": null
+      }]
